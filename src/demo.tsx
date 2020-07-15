bae51056401c464f162f15dfac3beeeb9663742e
mistlog <mistlog@outlook.com>
2020-07-15 22:25:34 +0800

    add debug dsl example

---
+++ src/demo.tsx
@@ -0,0 +1,7 @@
+let value = 1;
+
+{
+    "use debug";
+
+    console.log(`[Debug]: value is ${value}`);
+} 
--- typedraft.config.ts
+++ typedraft.config.ts
@@ -1,3 +1,33 @@
+import { Statement } from "@babel/types";
+import { IDSL } from "typedraft";
+
+class Debug implements IDSL {
+    m_Merge?: boolean;
+    m_Env: string;
+
+    constructor(option: { merge: boolean, env: string }) {
+        this.m_Merge = option?.merge;
+        this.m_Env = option?.env;
+    }
+
+    Transcribe(block: Array<Statement>): Array<Statement> {
+        if (this.m_Env === "dev") {
+            const [use_dsl, ...rest] = block;
+            return rest;
+        }
+        else if (this.m_Env === "production") {
+            return [];
+        }
+
+        throw new Error(`invalid env: ${this.m_Env}, please use dev or production`);
+    }
+}
+
 export default {
-    DSLs: []
+    DSLs: [
+        {
+            name: "debug",
+            dsl: () => new Debug({ merge: true, env: process.env.NODE_ENV || "dev" })
+        }
+    ]
 }
